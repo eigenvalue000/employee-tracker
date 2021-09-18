d4e1b7cf322d3af0b98f28b64959a48f8c8d7ad3
Garrett Kegel <ghkegel@gmail.com>
2021-09-18 11:30:04 -0700

    The root displays a list of all databases using a query statement SHOW databases. Next, the query will list information about the ems_db itself.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const sequelize = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -8,7 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to db and server
-sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '059Cy[0ebm1[',
+      database: 'ems_db'
+    },
+    console.log(`Connected to the ems_db database.`)
+  );
+
+  app.get('/', (req, res) => {
+      db.query('SHOW databases', 
+      function (err, results) {
+          res.json(results);
+      })
+  });
+
+  app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+  });
